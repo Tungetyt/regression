a988ab0e76cb9f03c12130b293383b2d6a3b6d14
MK <m0kotlicki@gmail.com>
2020-04-11 22:49:37 +0200

    simplified with methods and fixed wrong sinusoidal error calc bug

--- 02c_regresja.py
+++ 02c_regresja.py
@@ -10,6 +10,9 @@ def sinusoidal_equation(x):
 #     c = np.hstack([x, formula])
 #     v = np.linalg.pinv(c) @ y
 #     return v, sum((y - (v[0] * x + v[1])) ** 2) / x.size
+def calc_error(x, y, v, equation_result):
+    return sum((y - (v[0] * equation_result + v[1])) ** 2) / x.size
+
 
 def main():
 
@@ -32,12 +35,15 @@ def main():
 
     c2train = np.hstack([X_train, np.ones(X_train.shape)])
     v2train = np.linalg.pinv(c2train) @ y_train
-    e2train = sum((y_train - (v2train[0] * X_train + v2train[1])) ** 2) / X_train.size
+    # e2train = sum((y_train - (v2train[0] * X_train + v2train[1])) ** 2) / X_train.size
+    e2train = calc_error(X_train, y_train, v2train, X_train)
     print(f'linear training error: {e2train}')
 
     c2test = np.hstack([X_test, np.ones(X_test.shape)])
     v2test = np.linalg.pinv(c2test) @ y_test
-    e2test = sum((y_test - (v2test[0] * X_test + v2test[1])) ** 2) / X_test.size
+    # e2test = sum((y_test - (v2test[0] * X_test + v2test[1])) ** 2) / X_test.size
+    e2test = calc_error(X_test, y_test, v2test, X_test)
+
     print(f'linear testing error: {e2test}')
 
     print(f'linear error difference: {e2train[0] - e2test[0]}')
@@ -47,13 +53,15 @@ def main():
 
     c3train = np.hstack([sinusoidal_equation(X_train), np.ones(X_train.shape)])
     v3train = np.linalg.pinv(c3train) @ y_train
-    e3train = sum((y_train - (v3train[0] * X_train + v3train[1])) ** 2) / X_train.size
-
+    # e3train = sum((y_train - (v3train[0] * sinusoidal_equation(X_train) + v3train[1])) ** 2) / X_train.size
+    e3train = calc_error(X_train, y_train, v3train, sinusoidal_equation(X_train))
     print(f'sinusoidal training error: {e3train}')
+    # print(f'sinusoidal training error: {calc_error(X_train, y_train, v3train, sinusoidal_equation(X_train))}')
 
     c3test = np.hstack([sinusoidal_equation(X_test), np.ones(X_test.shape)])
     v3test = np.linalg.pinv(c3test) @ y_test
-    e3test = sum((y_test - (v3test[0] * X_test + v3test[1])) ** 2) / X_test.size
+    # e3test = sum((y_test - (v3test[0] * sinusoidal_equation(X_test) + v3test[1])) ** 2) / X_test.size
+    e3test = calc_error(X_test, y_test, v3test, sinusoidal_equation(X_test))
     print(f'sinusoidal testing error: {e3test}')
 
     print(f'sinusoidal error difference: {e3train[0] - e3test[0]}')
@@ -61,7 +69,6 @@ def main():
     plt.plot(x, y, 'ro')
     plt.plot(x, v3[0] * sinusoidal_equation(x) + v3[1])
     plt.plot(x, v2[0] * x + v2[1])
-
     plt.show()
 
 
